4b2ee3115d7e7e93200f0b16e6cc13bb50299aa0
eyuheli <yuhe.li@ericsson.com>
2013-09-27 16:56:16 +0800

    Learning string performance.

---
+++ src/banana/and/lighter/lang/StringImplInvest.java
@@ -0,0 +1,15 @@
+package banana.and.lighter.lang;
+
+public class StringImplInvest {
+
+	private static final String LETS_ROCK = "Let's rock.";
+	private static final String BREAK_UP = "Broke up with sb.";
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String test1 = "haha";
+		String test2 = LETS_ROCK + BREAK_UP;
+	}
+
+}
